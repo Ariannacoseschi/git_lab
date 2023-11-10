36500097c65dfaf4e92ab0cf2ea7f56b40a1873e
arianna coseschi <ariannacoseschi@gmail.com>
2023-11-10 12:06:10 +0000

    Merging with Comflicts

--- Greet.java
+++ Greet.java
@@ -1,10 +1,10 @@
 public class Greet {
 
-    public static void main(String[] args ){
+    public static void main(String[] args){
 
         System.out.println("Starting to Code in Style");
         System.out.println("We are finally understanding git");
-        
+        System.out.println("We got dis");
     }
 
 }
